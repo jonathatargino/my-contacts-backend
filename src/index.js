b7e272954338da3f490b035ace242c1956b90da6
Jonatha Targino <j.oliveiratargino@hotmail.com>
2023-06-30 10:20:38 -0300

    added eslint to project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-const express = require("express")
+const express = require("express");
 
-const app = express()
+const app = express();
 
 app.get("/", (req, res) => {
-  res.json({message: "Hello world"})
-})
+  res.json({ message: "Hello world" });
+});
 
-app.listen(3000, () => console.log("Servidor rodando"))
+app.listen(3000, () => console.log("Servidor rodando"));
