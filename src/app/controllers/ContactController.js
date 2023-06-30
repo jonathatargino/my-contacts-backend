5342b1b6e3418e1c09506ddfbf2810850029fc56
Jonatha Targino <j.oliveiratargino@hotmail.com>
2023-06-30 14:08:48 -0300

    create findById and delete routes using contacts mock data

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -6,8 +6,15 @@ class ContactController {
     res.json(contacts);
   }
 
-  show(req, res) {
+  async show(req, res) {
+    const { id } = req.params;
+    const contact = await ContactRepository.findById(id);
 
+    if (!contact) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    return res.json(contact);
   }
 
   store(req, res) {
@@ -18,8 +25,16 @@ class ContactController {
 
   }
 
-  delete(req, res) {
+  async delete(req, res) {
+    const { id } = req.params;
+    const contact = await ContactRepository.findById(id);
 
+    if (!contact) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    await ContactRepository.delete(id);
+    res.sendStatus(204);
   }
 }
 
--- src/app/repositories/ContactRepository.js
+++ src/app/repositories/ContactRepository.js
@@ -1,6 +1,6 @@
 const { uuid } = require("uuidv4");
 
-const contacts = [
+let contacts = [
   {
     id: uuid(),
     name: "José Lisboa",
@@ -35,6 +35,19 @@ class ContactRepository {
   findAll() {
     return new Promise((resolve) => {
       resolve(contacts);
+    });
+  }
+
+  findById(id) {
+    return new Promise((resolve) => {
+      resolve(contacts.find((contact) => contact.id === id));
+    });
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
     });
   }
 }
