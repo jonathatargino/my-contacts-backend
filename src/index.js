75a1459def82828d012370860f942841c56edae3
Jonatha Targino <j.oliveiratargino@hotmail.com>
2023-06-30 10:03:20 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require("express")
+
+const app = express()
+
+app.get("/", (req, res) => {
+  res.json({message: "Hello world"})
+})
+
+app.listen(3000, () => console.log("Servidor rodando"))
