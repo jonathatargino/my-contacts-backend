6ba22f4e03139f4cd6ccddf4322f7d61c01bebd8
Jonatha Targino <j.oliveiratargino@hotmail.com>
2023-06-30 11:02:00 -0300

    moved routes to new file routes.js, created ContactController

---
+++ src/app/controllers/ContactController.js
@@ -0,0 +1,23 @@
+class ContactController {
+  index(req, res) {
+    res.send("Send from Contact Controller");
+  }
+
+  show(req, res) {
+
+  }
+
+  store(req, res) {
+
+  }
+
+  update(req, res) {
+
+  }
+
+  delete(req, res) {
+
+  }
+}
+
+module.exports = new ContactController();
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 const express = require("express");
+const routes = require("./routes");
 
 const app = express();
-
-app.get("/", (req, res) => {
-  res.json({ message: "Hello world" });
-});
+app.use(routes);
 
 app.listen(3000, () => console.log("Servidor rodando"));
