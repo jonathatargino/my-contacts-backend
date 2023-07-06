d4caeb2003b080ea5fc65b364d3e7be666618109
Jonatha Targino <j.oliveiratargino@hotmail.com>
2023-07-06 16:42:12 -0300

    added cors & updated .env.example

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ require("dotenv").config();
 require("express-async-errors");
 
 const express = require("express");
+const cors = require("cors")
 const routes = require("./routes");
 
 const app = express();
+
+app.use(cors({origin: process.env.FRONTEND_URL}))
 
 app.use(express.json());
 app.use(routes);
@@ -13,4 +16,4 @@ app.use((error, request, response, next) => {
   response.sendStatus(500);
 });
 
-app.listen(3000, () => console.log("Listening routes"));
+app.listen(process.env.PORT, () => console.log("Listening routes"));
