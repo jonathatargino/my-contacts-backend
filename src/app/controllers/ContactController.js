c9c7bf5f9179038983f3d8251d2659390bfcf714
Jonatha Targino <j.oliveiratargino@hotmail.com>
2023-07-19 15:22:49 -0300

    ContactController and ContactRepository receiving user

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -4,19 +4,22 @@ const validate = require('uuid-validate');
 class ContactController {
   async index(req, res) {
     const { order } = req.query;
-    const contacts = await ContactRepository.findAll(order);
+    const {auth_user} = req
+
+    const contacts = await ContactRepository.findAll(order, auth_user.id);
     res.json(contacts);
   }
 
   async show(req, res) {
     const { id } = req.params;
+    const {auth_user} = req
 
     const isIdInvalid = !validate(id, 4)
     if (isIdInvalid) {
       return res.status(404).json({ error: "Contato não encontrado" });
     }
 
-    const contact = await ContactRepository.findById(id);
+    const contact = await ContactRepository.findById(id, auth_user.id);
 
 
     if (!contact) {
@@ -30,6 +33,7 @@ class ContactController {
     const {
       name, email, phone, category_id,
     } = req.body;
+    const {auth_user} = req
 
     const isCategoryIdInvalid = !validate(category_id, 4)
     if (isCategoryIdInvalid) {
@@ -40,14 +44,14 @@ class ContactController {
       return res.status(400).json({ error: "Nome é um campo obrigatório" });
     }
 
-    const contactExists = await ContactRepository.findByEmail(email);
+    const contactExists = await ContactRepository.findByEmail(email, auth_user.id);
 
     if (contactExists) {
       return res.status(400).json({ error: "Esse e-mail ja está em uso" });
     }
 
     const contact = await ContactRepository.create({
-      name, email, phone, category_id,
+      name, email, phone, category_id, user_id: auth_user.id
     });
 
     return res.status(201).json(contact);
@@ -55,6 +59,7 @@ class ContactController {
 
   async update(req, res) {
     const { id } = req.params;
+    const {auth_user} = req
 
     const isContactIdInvalid = !validate(id, 4)
     if (isContactIdInvalid) {
@@ -65,6 +70,7 @@ class ContactController {
       name, email, phone, category_id,
     } = req.body;
 
+
     const isCategoryIdInvalid = !validate(category_id, 4)
     if (isCategoryIdInvalid) {
       return res.status(404).json({ error: "Categoria não encontrado" });
@@ -74,20 +80,20 @@ class ContactController {
       return res.status(400).json({ error: "Nome é um campo obrigatório" });
     }
 
-    const contactExists = await ContactRepository.findById(id);
+    const contactExists = await ContactRepository.findById(id, auth_user.id);
 
     if (!contactExists) {
       return res.status(404).json({ error: "Contato não encontrado" });
     }
 
 
-    const contactByEmail = await ContactRepository.findByEmail(email);
+    const contactByEmail = await ContactRepository.findByEmail(email, auth_user.id);
 
     if (contactByEmail && contactByEmail.id !== id) {
       return res.status(400).json({ error: "Esse e-mail ja está em uso" });
     }
 
-    const updatedContact = await ContactRepository.update(id, {
+    const updatedContact = await ContactRepository.update(id, auth_user.id, {
       name, email, phone, category_id,
     });
 
@@ -96,19 +102,20 @@ class ContactController {
 
   async delete(req, res) {
     const { id } = req.params;
+    const {auth_user} = req
 
     const isIdInvalid = !validate(id, 4)
     if (isIdInvalid) {
       return res.status(404).json({ error: "Contato não encontrado" });
     }
 
-    const contact = await ContactRepository.findById(id);
+    const contact = await ContactRepository.findById(id, auth_user.id);
 
     if (!contact) {
       return res.status(404).json({ error: "Contato não encontrado" });
     }
 
-    await ContactRepository.delete(id);
+    await ContactRepository.delete(id, auth_user.id);
     return res.sendStatus(204);
   }
 }
